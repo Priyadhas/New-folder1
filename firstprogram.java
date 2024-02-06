5be0ac6a18d7f1dded7fff3888c2da10854746ff
PriyaDhas <mpriyad2001@gmail>
2024-02-06 12:10:18 +0530

    firstprogram

---
+++ firstprogram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class firstprogram 
+{
+    public static void main(String[] args)
+    {
+        Scanner s1=new Scanner (System.in);
+        int s=s1.nextInt();
+        if(s%2==0)
+        {
+
+        System.out.println("The first program is executed");
+        }
+        else
+        {
+            System.out.println("The first program is not executed");
+
+        }
+        
+    }
+}
